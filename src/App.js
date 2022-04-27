44f366f40c4e0cba67a2711c8ef67a603a0f80fd
eugenejac3 <eugene.jackson9@gmail.com>
2022-04-27 13:38:33 -0500

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/users/42")
+      .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setData(data)
+        })
+        
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p> {!data ? "Loading..." : data.firstName} </p>
+      <p> {!data ? "Loading..." : data.lastName} </p>
+      <p> {!data ? "Loading..." : data.age} </p>
     </div>
   );
 }
